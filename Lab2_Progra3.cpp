df03cbbab85d6d83a3443a22d8c00acc42b11f31
Mayra Salazar <mayrasalazar@unitec.edu>
2016-10-21 13:35:40 -0600

    Comenzando mmenú

--- Lab2_Progra3.cpp
+++ Lab2_Progra3.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
-
+int base(int);
+int decimal(int);
+int 
 int main(){
+	int opcion;
+	cout<<"----Menú-------\n 1)Ejercicio 1\n 2) Ejercicio 2\n 3) Ejercicio 3\n 4)Salir"<<endl;
+	cout<<"Ingrese un número: ";
 
+	return 0;
 }
