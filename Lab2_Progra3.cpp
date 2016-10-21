98eaa93de6affdc4ab3f4588c9a08a19c5a72ca9
Mayra Salazar <mayrasalazar@unitec.edu>
2016-10-21 13:41:33 -0600

    Menú Terminado

--- Lab2_Progra3.cpp
+++ Lab2_Progra3.cpp
@@ -6,7 +6,22 @@ int
 int main(){
 	int opcion;
 	cout<<"----Menú-------\n 1)Ejercicio 1\n 2) Ejercicio 2\n 3) Ejercicio 3\n 4)Salir"<<endl;
-	cout<<"Ingrese un número: ";
+	cout<<"Ingrese una opción: ";
+	cin>>opcion;
+	while(opcion!=4){
+		if (opcion == 1){
+			
+		}else if(opcion == 2){
+
+		}else if(opcion == 3){
+
+		}else{
+
+		}
+		cout<<"----Menú-------\n 1)Ejercicio 1\n 2) Ejercicio 2\n 3) Ejercicio 3\n 4)Salir"<<endl;
+		cout<<"Ingrese una opción: ";
+		cin>>opcion;
+	}
 
 	return 0;
 }
